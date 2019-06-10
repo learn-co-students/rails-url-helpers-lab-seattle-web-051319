16890f9ad80fd7c6fb5a6186ddb9bb76ccab3691
jjsmokovich <JesseSmokovich@yahoo.com>
2019-06-10 16:43:51 -0700

    Done.

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -2,7 +2,8 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :first_name
-      t.string :last_name
+      t.string :last_name 
+      t.boolean :active, :default => false
 
       t.timestamps null: false
     end
